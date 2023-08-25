7ad1c4275a0ad512ea87594e3c727555a3ab891f
Sivaka86 <143190785+Sivaka86@users.noreply.github.com>
2023-08-25 07:32:46 -0700

    first java program

---
+++ first.java
@@ -0,0 +1,19 @@
+package createpackage;//Package declaration 
+import java.io.Console;
+import java.lang.*; // import java.lang package
+
+// class declaration 
+public class FirstJavaProgram {
+
+	//main method declaration 
+	public static void main(String[] args) {
+		//print text
+		System.out.println("My First Java Program");// ln mean bring the cursor to the next line 
+		System.out.print("Learn core Java ");// without ln 
+    System.out.println("for automation");
+}
+
+/* output 
+  My First Java Program
+  Learn core Java for automation
+*/
