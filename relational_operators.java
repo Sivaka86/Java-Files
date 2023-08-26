cb835d2d6dac75095df5c5d90569c65820f5a104
Sivaka <143190785+Sivaka86@users.noreply.github.com>
2023-08-26 02:47:43 -0700

    Create relational_operators.java

---
+++ relational_operators.java
@@ -0,0 +1,15 @@
+class RelationalOperators{
+public static void main(String arg[])
+    {
+        int a=28;
+        int b=12;
+        System.out.println("Here  a == b = " + (a == b) ); //Here  a == b = false  
+        System.out.println("Here  a != b = " + (a != b) ); //Here  a != b = true
+        System.out.println("Here  a > b = " + (a > b) ); //Here  a > b = true
+        System.out.println("Here  a < b = " + (a < b) ); //Here  a < b = false
+        System.out.println("Here  b >= a = " + (b >= a) ); //Here  b >= a = false
+        System.out.println("Here  b <= a = " + (b <= a) ); //Here  b <= a = true    
+    }
+}
+
+
