5a010c881d7f4db0382e75a63e3c7f1ac470df98
Sivaka <143190785+Sivaka86@users.noreply.github.com>
2023-08-28 06:50:27 -0700

    Create string.java

---
+++ string.java
@@ -0,0 +1,7 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        String txt = "Hello World"; 
+        System.out.println(txt.toUpperCase());// Outputs "HELLO WORLD"
+        System.out.println(txt.toLowerCase());// Outputs "hello world"
+    }
+}
