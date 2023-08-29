981be0c6208d64fcdc1bf1ed3e593bc0a3cda9dc
Sivaka <143190785+Sivaka86@users.noreply.github.com>
2023-08-29 06:04:02 -0700

    Create method_ol.java

---
+++ method_ol.java
@@ -0,0 +1,15 @@
+public class main {
+    
+     static int plusM(int x,int y){
+        return x+y;
+    }
+    static double plusM(double x, double y){
+      return x+y;
+    }
+    public static void main(String[] args) {
+       double num1=plusM(3.2, 4.3);
+        int num2=plusM(2,6);
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+}
