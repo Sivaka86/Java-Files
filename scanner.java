6fc3525966606ba71299af58d9b676a780966dd8
Sivaka <143190785+Sivaka86@users.noreply.github.com>
2023-08-30 06:28:05 -0700

    Create scanner.java

---
+++ scanner.java
@@ -0,0 +1,16 @@
+class Main {
+    
+    
+    public static void main(String[] args) {
+       Scanner obj = new Scanner (System.in);// here the System.in taking the value 
+       System.out.println("Enter the 1st Number"+"\n");
+       int x = obj.nextInt();// nextInt reading the value
+      
+       System.out.println("Enter the 2st Number"+"\n");
+       int y = obj.nextInt();// nextInt reading the value
+  
+       System.out.println("Enter the 3rd Number"+"\n");
+       int z = obj.nextInt();// nextInt reading the value
+     
+    }
+}
