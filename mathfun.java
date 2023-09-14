e4222be501672a82ceda1c7ba9c107b4e6f7307a
Sivaka <143190785+Sivaka86@users.noreply.github.com>
2023-09-14 14:30:53 -0700

    Create mathfun.java

---
+++ mathfun.java
@@ -0,0 +1,16 @@
+public class Maths {
+
+	public static void main(String[] args) {
+		int i = 73;
+		int k = 27;
+		System.out.println(Math.max(i, k));
+		//System.out.println(Math.max(56, 89));
+		System.out.println(Math.min(i, k));
+		System.out.println(Math.sqrt(81));
+		System.out.println(Math.abs(-78));
+		System.out.println(Math.abs(96));
+		System.out.println(Math.random());
+		System.out.println(Math.random()*10000);
+	}
+
+}
