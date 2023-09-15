35776ccdf751024aec04d1e3e1a44b68748c4d28
Sivaka <143190785+Sivaka86@users.noreply.github.com>
2023-09-15 14:29:21 -0700

    Create booleans.java

---
+++ booleans.java
@@ -0,0 +1,21 @@
+class booleans {
+    public static void main(String[] args) {
+      
+       // Booleans datatype - true/ false (yes / no)
+       boolean x = true ;
+       boolean y =false ;
+       System.out.println(x);
+       System.out.println(y);
+       
+       // output
+       //true
+       //false
+       
+       //eg:2
+       //System.out.println(45<24);
+       //System.out.println(89>39);
+       //output
+       //false
+       //true
+    }
+}
