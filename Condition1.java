a5808af9d47ce8049b1eb97c924ba60ef0ef7b7f
Sivaka <143190785+Sivaka86@users.noreply.github.com>
2023-09-18 07:24:36 -0700

    Create Condition1.java

---
+++ Condition1.java
@@ -0,0 +1,17 @@
+public class Condition1 {
+
+	public static void main(String[] args) {
+		int a =78;
+		int b = 34;
+		if(a>b) {
+			System.out.println("a is greater than b ");
+	}
+		else if(a==b) {
+			System.out.println("a equal b");
+		}
+		
+		else {
+			System.out.println("a is less than b");
+		}
+	}
+}
