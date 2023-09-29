52383d3e10249ea51f87f7dcd2250ee4fdb8db1e
Sivaka <143190785+Sivaka86@users.noreply.github.com>
2023-09-29 10:38:38 -0700

    Create DateClass.java

---
+++ DateClass.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+class HelloWorld {
+    public static void main(String[] args) {
+      byte age = 40;
+      System.out.println("my age is : " + age);
+      Date today = new Date(); 
+      System.out.println(today);
+      //now.getTime();
+      
+     //output
+     //my age is : 40
+     //Fri Sep 29 17:36:17 GMT 2023
+
+    }
+}
