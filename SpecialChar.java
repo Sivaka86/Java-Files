2d16d1e01c00b20ed66e42c1ebe2ff69ab8f0558
Sivaka <143190785+Sivaka86@users.noreply.github.com>
2023-09-30 04:49:20 -0700

    Create SpecialChar.java

---
+++ SpecialChar.java
@@ -0,0 +1,13 @@
+class SpecialChar {
+    public static void main(String[] args) {
+        // include special characters  
+        // include " "
+        System.out.println("Hello, \"World!\" ");
+        //c:\windows\...  , include \ 
+         System.out.println("c:\\windows\\...");
+         // include line break \u
+         System.out.println("c:\nwindows\\...");
+         //tab
+         System.out.println("My name is :\tSivaka");
+    }
+}
